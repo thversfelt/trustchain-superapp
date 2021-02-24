74e1a4da223a2d4db34e959093bf3f106efbd6f7
Emiel Bos <emielbos@protonmail.com>
2021-02-24 17:30:44 +0100

    Hierebaan

--- liquiditypool/src/main/java/tudelft/liquiditypool/pool/LiquidityPool.kt
+++ liquiditypool/src/main/java/tudelft/liquiditypool/pool/LiquidityPool.kt
@@ -6,11 +6,13 @@ import tudelft.liquiditypool.sharedwallet.SharedWallet
 class LiquidityPool(sharedWallets: Pair<SharedWallet, SharedWallet>) {
 
     val sharedWallets: Pair<SharedWallet, SharedWallet>
+    val amounts: Pair<Double, Double>
 
     init {
         Log.i("Pool", "Pool: attempting to start.")
         // Broadcast to trustchain
         this.sharedWallets = sharedWallets
+        this.amounts = Pair<Double, Double>(0.0, 0.0)
     }
 
     fun join(key: String) {
@@ -27,8 +29,14 @@ class LiquidityPool(sharedWallets: Pair<SharedWallet, SharedWallet>) {
         return 69.69
     }
 
-    fun swap(index: Int, value: Double) { // the parameters refer to the user's offer; index is which coin he/she submits and value indicates how much
+    fun swap(index: Int, amount: Double) { // the parameters refer to the user's offer; index is which coin he/she submits and value indicates how much
 
     }
+
+    fun provide(amounts: Pair<Double, Double>) {
+
+    }
+
+    fun reclaim()
 }
 
